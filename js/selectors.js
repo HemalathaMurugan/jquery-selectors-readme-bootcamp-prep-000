f7b9a04c2267642ddc85ed11447e6aff845f991d
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-28 02:23:23 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,11 +1,12 @@
 // declare your functions here...
-$(document.ready(function paragraphSelector(){
+$(document.ready());
+function paragraphSelector(){
   let par=$('p');
   return par;
-},
+}
 function lastImageSelector(){
   let last=$('img:last');
   return last;
 }
 
-));
+
